70583bf38ed0461bd249fe151d69a4e2a77c9520
zhouah <yqzhouah@foxmail.com>
2024-04-25 19:04:05 +0800

    feat: 窗口按键功能实现

--- src/renderer/src/components/Header/index.jsx
+++ src/renderer/src/components/Header/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import styles from './styles.module.scss'
 import Ico from '../Icos'
 
@@ -9,12 +10,49 @@ const HeaderLeft = () => {
 		</div>
 	)
 }
-
 const HeaderRight = () => {
+	const [isMaximize, setIsMaximize] = useState(false)
+
+	const handleMinimize = () => {
+		window.api.minimize()
+	}
+
+	const handleClose = () => {
+		window.api.close()
+	}
+
+	const handleMaximize = () => {
+		setIsMaximize(true)
+		window.api.maximize()
+	}
+
+	const handleDefaultScreen = () => {
+		setIsMaximize(false)
+		window.api.defaultScreen()
+	}
+
+	const fullOrDefaultScreen = () => {
+		if (!isMaximize) {
+			return (
+				<Ico icoType={'fullscreen'} size={24} isPointer={true} onClick={handleMaximize} />
+			)
+		} else {
+			return (
+				<Ico
+					icoType={'defaultscreen'}
+					size={24}
+					isPointer={true}
+					onClick={handleDefaultScreen}
+				/>
+			)
+		}
+	}
+
 	return (
 		<div className={styles['header-right']}>
-			<Ico icoType={'fullscreen'} size={24} isPointer={true} />
-			<Ico icoType={'close'} size={24} />
+			<Ico icoType={'minimize'} size={24} isPointer={true} onClick={handleMinimize} />
+			{fullOrDefaultScreen()}
+			<Ico icoType={'close'} size={24} isPointer={true} onClick={handleClose} />
 		</div>
 	)
 }
